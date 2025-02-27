a7c2463c2ad253d40e4183ff814094e7be2feceb
vikas054 <vikaschauhan@gmail.com>
2025-02-27 08:17:38 +0530

    initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Main{
+public static void main(String[] args) {
+ System.out.println("This is java program");   
+ System.out.println("output from java");
+ System.out.println(new Date());
+}
+}
